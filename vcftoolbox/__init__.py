e0a3ad74f210521ff276fa926c90cb1b98ca99f2
moonso <monsunas@gmail.com>
2015-11-17 14:23:51 +0100

    Added add_vcf_info

--- vcftoolbox/__init__.py
+++ vcftoolbox/__init__.py
@@ -4,5 +4,5 @@ from .header_parser import HeaderParser
 from .genotype import Genotype
 from .parse_variant import (get_variant_dict, get_info_dict, get_variant_id, 
 get_vep_dict)
-from .add_variant_information import (replace_vcf_info)
+from .add_variant_information import (replace_vcf_info, add_vcf_info)
 from .prints import (print_headers, print_variant)
